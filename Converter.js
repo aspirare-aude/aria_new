66153fbd2317656a512d56ee9539d21ea995ae3b
Tomas O Mahony <tomasmahony@gmail.com>
2014-02-26 20:55:06 +0000

    lksjkdsf

--- Converter.js
+++ Converter.js
@@ -5,12 +5,20 @@ var Converter = (function () {
  return {
 
  convertFromImperialToMetric: function (weight) {
+ 	var num parseInt(weight);
+
+ 	if(isNaN(num) == true) {
+ 		throw new Error ("not a number");
+ 	}
+ 	num = num / converterValue;
+ 	return Math.round(num);
 
  },
 
  convertFromMetricToImperial: function (weight) {
 
  }
+ 	return weight/converterValue;
 
  }
 
