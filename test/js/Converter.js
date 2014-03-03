ff7b97baa8c9731d438bda8045748a159f3257ee
Tomas O Mahony <tomasmahony@gmail.com>
2014-03-03 18:49:18 +0000

    responsive

---
+++ test/js/Converter.js
@@ -0,0 +1,26 @@
+var Converter = (function () {
+
+ var converterValue = 2.2;
+
+ return {
+
+ convertFromImperialToMetric: function (weight) {
+ 	var num = parseInt(weight);
+
+ 	if(isNaN(num) == true) {
+ 		throw new Error ("Not a number");
+ 	}
+ 	num = num / converterValue;
+ 	return Math.round(num);
+
+ },
+
+ convertFromMetricToImperial: function (weight) {
+ 	 	return weight / converterValue;
+
+
+ }
+
+ }
+
+})();
